101b6d92000cecc3f6f8ea3d607a00b2a2deac71
Jason Barrie Morley <jbmorley@mac.com>
2022-11-27 08:41:06 +0000

    feat: Add `ConditionalHeader` and `ConditionalTitle` for managing dynamic navigation bar titles

---
+++ Sources/Interact/View Modifiers/ConditionalTitle.swift
@@ -0,0 +1,53 @@
+// Copyright (c) 2018-2022 InSeven Limited
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import SwiftUI
+
+import Foundation
+
+public struct ConditionalTitle<Title: View>: ViewModifier {
+
+    private var isVisible: Bool
+    private let title: Title
+
+    public init(_ isVisible: Bool, @ViewBuilder title: () -> Title) {
+        self.isVisible = isVisible
+        self.title = title()
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    title
+                        .opacity(isVisible ? 1.0 : 0.0)
+                }
+            }
+    }
+
+}
+
+extension View {
+
+    public func conditionalTitle<Title: View>(_ isVisible: Bool, @ViewBuilder title: () -> Title) -> some View {
+        return modifier(ConditionalTitle(isVisible, title: title))
+    }
+
+}
