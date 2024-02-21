2778d1e46387465019142e22755a4eedb11d85fd
Jason Morley <hello@jbmorley.co.uk>
2024-02-20 22:18:39 -1000

    fix: Export the license using the new `Licensable` protocol (#87)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,20 +9,20 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Interact",
             targets: ["Interact"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/inseven/licensable", from: "0.0.9"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Interact",
-            dependencies: [],
+            dependencies: [
+                .product(name: "Licensable", package: "licensable"),
+            ],
             resources: [.process("Resources")]),
         .testTarget(
             name: "InteractTests",
