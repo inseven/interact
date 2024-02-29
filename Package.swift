ef198760801b262e2dcf75ece554eebfe8ad3f75
Jason Morley <hello@jbmorley.co.uk>
2024-02-29 11:48:49 -1000

    fix: Update minimum version of Licensable to 0.0.13
    This addresses an issue where Licensable renamed the `License` constructor parameters.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/inseven/licensable", from: "0.0.11"),
+        .package(url: "https://github.com/inseven/licensable", from: "0.0.13"),
     ],
     targets: [
         .target(
