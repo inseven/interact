e375f52aa68186c367b27cd90dff21966924dc9d
Jason Morley <hello@jbmorley.co.uk>
2024-03-01 15:19:16 -1000

    fix: Introduce basic keyed keychain wrapper

---
+++ Sources/Interact/Utilities/KeychainManager.swift
@@ -0,0 +1,79 @@
+// Copyright (c) 2018-2024 Jason Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+public struct KeychainManager<Key: RawRepresentable> where Key.RawValue == String {
+
+    public init() {
+        
+    }
+
+    func delete(key: Key) throws {
+        let query: [String: Any] = [
+            kSecAttrSynchronizable as String: kCFBooleanTrue,
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key.rawValue,
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess else {
+            throw KeychainError(status: status)
+        }
+    }
+
+    public func set(_ value: String?, forKey key: Key) throws {
+        if let value {
+            let data = value.data(using: .utf8)!
+            let query: [String: Any] = [
+                kSecAttrSynchronizable as String: kCFBooleanTrue,
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrAccount as String: key.rawValue,
+                kSecValueData as String: data
+            ]
+            SecItemDelete(query as CFDictionary)
+            let status = SecItemAdd(query as CFDictionary, nil)
+            guard status == errSecSuccess else {
+                throw KeychainError(status: status)
+            }
+        } else {
+            try delete(key: key)
+        }
+    }
+
+    public func string(forKey key: Key) throws -> String? {
+        let query: [String: Any] = [
+            kSecAttrSynchronizable as String: kCFBooleanTrue,
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key.rawValue,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var item: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess else {
+            throw KeychainError(status: status)
+        }
+        guard let data = item as? Data else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+
+}
