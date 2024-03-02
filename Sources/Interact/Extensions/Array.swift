c36e5f6f7200e32d9c90d885f7eeb69206a70226
Jason Morley <hello@jbmorley.co.uk>
2024-03-02 11:58:20 -1000

    fix: Add async `map` to Array (#96)

---
+++ Sources/Interact/Extensions/Array.swift
@@ -0,0 +1,40 @@
+// Copyright (c) 2018-2024 Jason Morley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+extension Array {
+
+    public func map<T>(_ transform: @escaping (Element) async throws -> T) async rethrows -> [T] {
+        return try await withThrowingTaskGroup(of: T.self) { group in
+            for element in self {
+                group.addTask {
+                    return try await transform(element)
+                }
+            }
+            var items = [T]()
+            for try await item in group {
+                items.append(item)
+            }
+            return items
+        }
+    }
+
+}
